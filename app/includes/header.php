eb3caf6a41c0230e7cbfc5846043fa900cc488f0
Luke Tainton <luke@tainton.uk>
2020-08-04 21:20:57 +0100

    :construction: Did some stuff, don't remember exactly what
    Signed-off-by: Luke Tainton <luke@tainton.uk>

--- app/includes/header.php
+++ app/includes/header.php
@@ -9,11 +9,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="theme-color" content="#563d7c">
-    <?php
-      if (!is_signed_in()) {
-        echo("<meta http-equiv='refresh' content='0; url=" . $_ENV['APP_URL'] . "/login' />");
-      }
-    ?>
     <title><?php echo( $PAGE_TITLE ); ?></title>
 
     <!-- Bootstrap core CSS -->
