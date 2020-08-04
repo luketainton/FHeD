d627caad57619ee4f4e4aaf071aab0f5cf110785
Alexander Davis <alex@adcm.uk>
2020-08-04 15:33:28 +0100

    New Request Form

--- app/public/new.php
+++ app/public/new.php
@@ -8,7 +8,10 @@
     }
 ?>
 
-
+<link rel="stylesheet" href="fonts/fontawesome-all.min.css">
+<link rel="stylesheet" href="css/Contact-Form-Clean.css">
+<link rel="stylesheet" href="css/Custom-File-Upload.css">
+<link rel="stylesheet" href="css/styles.css">
 
 <!-- Begin page content -->
 <main role="main" class="flex-shrink-0">
@@ -23,7 +26,17 @@
   </section>
 
     <section>
-      <p>This page is currently under construction.</p>
+      <div class="contact-clean">
+          <form method="post">
+              <div class="form-group"><input class="form-control" type="text" name="subject" placeholder="Subject"></div>
+              <div class="form-group"><textarea class="form-control" name="message" placeholder="Message" rows="14"></textarea></div>
+              <div class="form-group"><input type="file" id="user_group_logo" class="custom-file-input" accept="*" name="user_group_logo"><label id="user_group_label" for="user_group_logo"><i class="fas fa-upload"></i>&nbsp;Choose file(s)...</label>
+                  <div class="text-center mt-2"><button class="btn btn-primary" type="submit">Upload</button></div>
+                  <div class="text-center mt-2"><button class="btn btn-primary" type="submit">send </button></div>
+              </div>
+          </form>
+      </div>
+      <script src="assets/js/Custom-File-Upload.js"></script>
     </section>
 
 </main>
