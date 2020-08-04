67ea371cc9f7c1cd35155b2f88368b214eed1cfe
Luke Tainton <luke@tainton.uk>
2020-08-04 19:32:18 +0100

    :bug: Force remove headers before redirection
    Signed-off-by: Luke Tainton <luke@tainton.uk>

--- app/includes/header.php
+++ app/includes/header.php
@@ -67,3 +67,14 @@
         </div>
     </nav>
     </header>
+
+    <section id="alerts">
+      <?php
+        foreach($_SESSION['alerts'] as $alert) {
+          echo("
+          <div class='alert alert-" . $alert['type'] . "' role='alert'>
+            " . $alert['msg'] . "
+          </div>
+        ") }
+      ?>
+    </section>
