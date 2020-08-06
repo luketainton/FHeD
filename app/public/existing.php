c69d2a1609f267af3514e584ef0f84c91c139e03
Luke Tainton <luke@tainton.uk>
2020-08-06 18:23:22 +0100

    :sparkles: Add existing tickets
    Signed-off-by: Luke Tainton <luke@tainton.uk>

--- app/public/existing.php
+++ app/public/existing.php
@@ -1,6 +1,34 @@
 <?php
     $PAGE_NAME = "Existing requests";
+    require_once __DIR__ . "/../includes/prereqs.php";
     require_once __DIR__ . "/../includes/header.php";
+
+    if (is_signed_in()) {
+      // Get user's open tickets
+      try {
+        $user_tickets_stmt = "SELECT uuid, id, title, description, status FROM tickets WHERE created_by=:uuid";
+        $user_tickets_sql = $db->prepare($user_tickets_stmt);
+        $user_tickets_sql->bindParam(':uuid', $_SESSION['uuid']);
+        $user_tickets_sql->execute();
+        $user_tickets_sql->setFetchMode(PDO::FETCH_ASSOC);
+        $user_tickets_result = $user_tickets_sql->fetchAll();
+      } catch (PDOException $e) {
+        echo("Error: " . $e->getMessage());
+      }
+
+      // Get user's closed tickets
+      try {
+        $closed_tickets_stmt = "SELECT uuid, id, title, description, status FROM tickets WHERE created_by=:uuid AND status='closed'#";
+        $closed_tickets_sql = $db->prepare($user_tickets_stmt);
+        $closed_tickets_sql->bindParam(':uuid', $_SESSION['uuid']);
+        $closed_tickets_sql->execute();
+        $closed_tickets_sql->setFetchMode(PDO::FETCH_ASSOC);
+        $closed_tickets_result = $user_tickets_sql->fetchAll();
+      } catch (PDOException $e) {
+        echo("Error: " . $e->getMessage());
+      }
+
+      }
 ?>
 
 
@@ -20,6 +48,105 @@
 
       <section>
         <p>This page is currently under construction.</p>
+      </section>
+    <?php } else { ?>
+      <section>
+        <div class='alert alert-danger alert-dismissible fade show' role='alert'>
+          You need to log in to access this page.
+          <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+            <span aria-hidden='true'>&times;</span>
+          </button>
+        </div>
+      </section>
+    <?php } ?>
+
+</main>
+
+<?php
+    require_once __DIR__ . "/../includes/footer.php";
+?>
+
+
+
+
+
+
+
+
+
+
+
+<!-- Begin page content -->
+<main role="main" class="flex-shrink-0">
+
+  <section class="jumbotron text-center">
+    <div class="container">
+      <h1>Open requests</h1>
+      <p class="lead text-muted">
+        Here you can find all of your requests, and other requests that you are subscribed to.
+      </p>
+    </div>
+  </section>
+
+  <?php if (is_signed_in()) { ?>
+    <section>
+      <div class="card mx-auto" style="width: 80%;">
+        <div class="card-header">
+          <span class="mdi mdi-ticket-outline"></span> My Open Requests
+        </div>
+        <ul class="list-group list-group-flush">
+          <?php
+            if (count($user_tickets_result) == 0) {
+              echo("<center><b>No open tickets</b></center>");
+            } else {
+              foreach($user_tickets_result as $tkt) {
+          ?>
+          <li class="list-group-item">
+            <div class="container">
+              <div class="row">
+                <div class="col-10">
+                  <span style="display: inline;" class="text-muted">#<?php echo(sprintf("%'.05d\n", $tkt["id"])); ?> </span><span><b><?php echo($tkt['title']); ?></b></span>
+                  <p class="m-0"><?php echo($tkt['description']); ?></p>
+                </div>
+                <div class="col-2">
+                  <a class="btn btn-success float-right" href="view?rid=<?php echo($tkt["uuid"]); ?>" role="button">Go</a>
+                </div>
+              </div>
+            </div>
+          </li>
+          <?php } } ?>
+        </ul>
+      </div>
+    </section>
+
+      <section>
+        <div class="card mx-auto" style="width: 80%;">
+          <div class="card-header">
+            <span class="mdi mdi-ticket-outline"></span> My Closed Requests
+          </div>
+          <ul class="list-group list-group-flush">
+            <?php
+              if (count($closed_tickets_result) == 0) {
+                echo("<center><b>No closed tickets</b></center>");
+              } else {
+                foreach($closed_tickets_result as $tkt) {
+            ?>
+            <li class="list-group-item">
+              <div class="container">
+                <div class="row">
+                  <div class="col-10">
+                    <span style="display: inline;" class="text-muted">#<?php echo(sprintf("%'.05d\n", $tkt["id"])); ?> </span><span><b><?php echo($tkt['title']); ?></b></span>
+                    <p class="m-0"><?php echo($tkt['description']); ?></p>
+                  </div>
+                  <div class="col-2">
+                    <a class="btn btn-success float-right" href="view?rid=<?php echo($tkt["uuid"]); ?>" role="button">Go</a>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <?php } } ?>
+          </ul>
+        </div>
       </section>
     <?php } else { ?>
       <section>
