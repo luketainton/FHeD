aa3de0315864a0120bc204a516745f28eca37567
Alexander Davis <alexander@davisonline.uk>
2020-08-06 18:47:55 +0100

    Update existing.php

--- app/public/existing.php
+++ app/public/existing.php
@@ -36,49 +36,6 @@
 <!-- Begin page content -->
 <main role="main" class="flex-shrink-0">
 
-  <?php if (is_signed_in()) { ?>
-    <section class="jumbotron text-center">
-      <div class="container">
-        <h1>Open requests</h1>
-        <p class="lead text-muted">
-          Here you can find all of your requests, and other requests that you are subscribed to.
-        </p>
-      </div>
-    </section>
-
-      <section>
-        <p>This page is currently under construction.</p>
-      </section>
-    <?php } else { ?>
-      <section>
-        <div class='alert alert-danger alert-dismissible fade show' role='alert'>
-          You need to log in to access this page.
-          <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-            <span aria-hidden='true'>&times;</span>
-          </button>
-        </div>
-      </section>
-    <?php } ?>
-
-</main>
-
-<?php
-    require_once __DIR__ . "/../includes/footer.php";
-?>
-
-
-
-
-
-
-
-
-
-
-
-<!-- Begin page content -->
-<main role="main" class="flex-shrink-0">
-
   <section class="jumbotron text-center">
     <div class="container">
       <h1>Open requests</h1>
