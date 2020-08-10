c7dcf6235335c3abebdd4111b685e80842b21534
Alexander Davis <alex@adcm.uk>
2020-08-10 14:16:52 +0100

    Fixed broken existing

--- app/public/existing.php
+++ app/public/existing.php
@@ -3,11 +3,14 @@
     require_once __DIR__ . "/../includes/header.php";
 
     if (is_signed_in()) {
-      $open_requests = array();
-      $closed_requests = array();
+      $closed_requests =  get_my_open_requests($db);
       $subscriptions = get_subscribed_requests($db);
+<<<<<<< Updated upstream
 
       $requests = get_my_requests($db);
+=======
+      $open_requests = get_my_open_requests($db);
+>>>>>>> Stashed changes
 
       foreach($requests as $req) {
         if ($req['status'] != "Closed") {
