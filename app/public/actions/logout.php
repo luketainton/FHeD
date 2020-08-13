d5c8ae1c23cb8d33ff16e535426622135bdcd520
Alexander Davis <AlexanderEDavis@users.noreply.github.com>
2020-08-13 20:18:35 +0100

    Merge Main with feature/71 (#81)
    * Possible login fix (#72)
    
    Change checks to empty() and movement of try statements
    
    * Adding statement brackets (#73)
    
    * :lock: Force Single Logout (#77)
    
    Signed-off-by: Luke Tainton <luke@tainton.uk>
    
    * Update assigned-issues-inprogress.yml
    
    Co-authored-by: Luke Tainton <luke@tainton.uk>

--- app/public/actions/logout.php
+++ app/public/actions/logout.php
@@ -2,7 +2,7 @@
     $PAGE_NAME = "Logging out...";
     require_once __DIR__ . "/../../includes/prereqs.php";
 
+    $access_token = $_SESSION['access_token'];
     session_destroy();
-
-    header('Location: /');
+    $oidc->signOut($access_token, $_ENV['APP_URL']);
 ?>
