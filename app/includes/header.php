dc4664a1aff876f599eb50ea43addfebb5766c82
Alexander Davis <alex@adcm.uk>
2020-08-14 21:18:54 +0100

    Removal of duplicate references

--- app/includes/header.php
+++ app/includes/header.php
@@ -16,9 +16,6 @@
     <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.materialdesignicons.com/5.3.45/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="/css/custom.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   </head>
 
   <body class="d-flex flex-column h-100">
