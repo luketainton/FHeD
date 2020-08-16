d574e6b2fec015dd0e4fa025747522901f777484
Luke Tainton <luke@tainton.uk>
2020-08-16 18:41:30 +0100

    :sparkles: Move mailing list to Google Groups (#101)
    * :sparkles: Move mailing list to Google Groups
    
    * Restyled by php-cs-fixer (#102)
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -1,19 +1,41 @@
 <footer class="footer mt-auto py-3">
   <div class="text-center text-muted">
     <?php
-      echo( $_ENV['APP_NAME'] . " v" . $_ENV['APP_VERSION']);
-      if ($_ENV['APP_NAME'] != "FHeD") {echo(", powered by FHeD");};
+      echo($_ENV['APP_NAME'] . " v" . $_ENV['APP_VERSION']);
+      if ($_ENV['APP_NAME'] != "FHeD") {
+          echo(", powered by FHeD");
+      };
     ?><br>
-    <?php if (is_signed_in()) { ?>
-      <form id="mailer-subscribe" method="post" action="https://mailer.tainton.uk/subscription/form" style="margin-top: -1%;">
-        <div class="form-group">
-          <input hidden type="text" class="form-control" id="name" name="name" value="<?php echo($_SESSION['full_name']); ?>">
-          <input hidden type="text" class="form-control" id="email" name="email" value="<?php echo($_SESSION['email']); ?>">
-          <input hidden type="text" class="form-control" id="bdce4" name="l" value="bdce4805-ff03-41f1-be2c-60b28c444e83">
+    <?php if (is_signed_in()) {
+        ?>
+      <a data-target="#mailingListModal" data-toggle="modal" href="#mailingListModal">Subscribe</a>
+      <div class="modal fade" id="mailingListModal" tabindex="-1" aria-labelledby="mailingListModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="mailingListModalLabel">FHeD Users mailing list</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+            <p>
+              The <i>FHeD Users</i> mailing list is aimed at administrators managing an FHeD deployment, allowing you to interact
+              with other FHeD administrators and get help from the developers.
+            </p>
+            <p>
+              <b>You need a Google account to subscribe.</b> Your data will be stored in accordance with Google's <a target="_blank" href="https://policies.google.com/privacy">privacy policy</a>.
+            </p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+              <a href='https://groups.google.com/g/fhed-users' class='btn btn-success'>Subscribe</a>
+            </div>
+          </div>
         </div>
-        <a href="#" onclick="document.getElementById('mailer-subscribe').submit();">Subscribe</a>
-      </form>
-    <?php } ?>
+      </div>
+    <?php
+    } ?>
   </div>
 </footer>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
