7d38a8007187d63459607060df204dbe0420be91
Restyled.io <commits@restyled.io>
2020-08-17 14:44:58 +0000

    Restyled by php-cs-fixer

--- app/includes/app_functions.php
+++ app/includes/app_functions.php
@@ -8,15 +8,14 @@
 
   function user_exists($db, $uuid)
   {
-    try {
-        $sql = $db->prepare("SELECT uuid FROM users WHERE uuid=:uuid");
-        $sql->bindParam(':uuid', $uuid);
-        $sql->execute();
+      try {
+          $sql = $db->prepare("SELECT uuid FROM users WHERE uuid=:uuid");
+          $sql->bindParam(':uuid', $uuid);
+          $sql->execute();
       } catch (PDOException $e) {
-        $alert = array("danger", "Error during check for user record: " . $e->getMessage());
+          $alert = array("danger", "Error during check for user record: " . $e->getMessage());
       }
-      if (empty($sql))
-      {
+      if (empty($sql)) {
           return false;
       } else {
           return true;
